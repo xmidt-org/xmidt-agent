9abdaa8993d992d38abccecd952ae7d7b6931c37
schmidtw <Weston_Schmidt@alumni.purdue.edu>
2024-07-23 18:14:52 -0700

    chore:Add user and group for xmidt-agent, as well as set the service to preset (enabled).

---
+++ .release/ipk/postinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2024 Comcast Cable Communications Management, LLC
+# SPDX-License-Identifier: Apache-2.0
+
+systemctl preset xmidt-agent.service >/dev/null 2>&1
